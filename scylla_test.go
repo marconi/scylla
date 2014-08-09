b32e115691e655962aa724644819fc173d40b9c3
Marconi Moreto <caketoad@gmail.com>
2014-08-09 13:36:29 +0800

    speed-up improvements for tests

--- scylla_test.go
+++ scylla_test.go
@@ -1,4 +1,4 @@
-package scylla_test
+package scylla
 
 import (
 	"testing"
@@ -6,7 +6,6 @@ import (
 
 	"github.com/garyburd/redigo/redis"
 	. "github.com/marconi/rivers"
-	. "github.com/marconi/scylla"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -15,8 +14,6 @@ func TestStatsLogging(t *testing.T) {
 		conn := Pool.Get()
 
 		Convey("push without delay", func() {
-			min := time.Now().UTC().Unix()
-
 			uq1 := NewQueue("uq1", "urgent")
 			logger1 := NewStatsLogger(uq1)
 			logger1.Bind()
@@ -25,21 +22,16 @@ func TestStatsLogging(t *testing.T) {
 			uq1.Push(NewJob())
 
 			now := time.Now().UTC().Unix()
-			diff := now - min
+			logger1.flush(now)
 
-			// check that before min up to max,
-			// there shouldn't be any stats logged
-			for i := int64(0); i <= diff; i++ {
-				sec := min + i
-				secKey := SecStatsKey(PushStatsKey(uq1.GetName()), sec)
-				exists, _ := redis.Bool(conn.Do("EXISTS", secKey))
-				So(exists, ShouldEqual, false)
+			secKey := SecStatsKey(PushStatsKey(uq1.GetName()), now)
+			exists, _ := redis.Bool(conn.Do("EXISTS", secKey))
+			So(exists, ShouldEqual, false)
 
-				// there should be no queue size logged as well
-				sizeKey := SecQueueSizeKey(uq1.GetName(), sec)
-				exists, _ = redis.Bool(conn.Do("EXISTS", sizeKey))
-				So(exists, ShouldEqual, false)
-			}
+			// there should be no queue size logged as well
+			sizeKey := SecQueueSizeKey(uq1.GetName(), now)
+			exists, _ = redis.Bool(conn.Do("EXISTS", sizeKey))
+			So(exists, ShouldEqual, false)
 
 			Reset(func() {
 				uq1.Destroy()
@@ -47,53 +39,38 @@ func TestStatsLogging(t *testing.T) {
 			})
 		})
 
-		Convey("push with 2 seconds delay", func() {
-			min := time.Now().UTC().Unix()
-
+		Convey("push with delay", func() {
 			uq2 := NewQueue("uq2", "urgent")
 			logger2 := NewStatsLogger(uq2)
 			logger2.Bind()
-
 			uq2.Push(NewJob())
 
-			// sleep two seconds to trigger a flush on next push
-			time.Sleep(2 * time.Second)
-
-			uq2.Push(NewJob())
-
+			time.Sleep(1 * time.Millisecond)
 			now := time.Now().UTC().Unix()
-			diff := now - min
+			logger2.flush(now + 2)
 
-			// sleep one more to make sure flush is done
-			// before we actually start checking
-			time.Sleep(1 * time.Second)
-
-			// check the before min up to max,
-			// there should be stats logged
 			foundSecKey := false
 			foundSizeKey := false
-			for i := int64(0); i <= diff; i++ {
-				sec := min + i
-				secKey := SecStatsKey(PushStatsKey(uq2.GetName()), sec)
-				secExists, _ := redis.Bool(conn.Do("EXISTS", secKey))
-				if secExists {
-					foundSecKey = true
 
-					pushStats, _ := redis.Int64(conn.Do("GET", secKey))
-					So(pushStats, ShouldEqual, 1)
-				}
+			secKey := SecStatsKey(PushStatsKey(uq2.GetName()), now)
+			secExists, _ := redis.Bool(conn.Do("EXISTS", secKey))
+			if secExists {
+				foundSecKey = true
 
-				// there should be queue size logged as well
-				sizeKey := SecQueueSizeKey(uq2.GetName(), sec)
-				sizeExists, _ := redis.Bool(conn.Do("EXISTS", sizeKey))
-				if sizeExists {
-					foundSizeKey = true
+				pushStats, _ := redis.Int64(conn.Do("GET", secKey))
+				So(pushStats, ShouldEqual, 1)
+			}
 
-					// due to the sleep we can be sure that both items
-					// has been flushed so we check for size of two here
-					sizeStats, _ := redis.Int64(conn.Do("GET", sizeKey))
-					So(sizeStats, ShouldEqual, 2)
-				}
+			// there should be queue size logged as well
+			sizeKey := SecQueueSizeKey(uq2.GetName(), now)
+			sizeExists, _ := redis.Bool(conn.Do("EXISTS", sizeKey))
+			if sizeExists {
+				foundSizeKey = true
+
+				// due to the sleep we can be sure that both items
+				// has been flushed so we check for size of two here
+				sizeStats, _ := redis.Int64(conn.Do("GET", sizeKey))
+				So(sizeStats, ShouldEqual, 1)
 			}
 
 			So(foundSecKey, ShouldEqual, true)
@@ -106,8 +83,6 @@ func TestStatsLogging(t *testing.T) {
 		})
 
 		Convey("with pop stats", func() {
-			min := time.Now().UTC().Unix()
-
 			uqdq1 := NewQueue("uqdq1", "delayed")
 			uq3 := NewQueue("uq3", "urgent")
 
@@ -131,47 +106,34 @@ func TestStatsLogging(t *testing.T) {
 			_, err = uq3.Push(j2)
 			So(err, ShouldEqual, nil)
 
-			// sleep two seconds to trigger a flush on next push
-			time.Sleep(2 * time.Second)
-
-			// that would be 3 push and 2 pop jobs flushed
-			uqdq1.Push(NewJob())
-
+			time.Sleep(1 * time.Millisecond)
 			now := time.Now().UTC().Unix()
-			diff := now - min
+			logger3.flush(now + 2)
 
-			// sleep one more to make sure flush is done
-			// before we actually start checking
-			time.Sleep(1 * time.Second)
-
-			// check the before min up to max,
-			// there should be stats logged for pop
 			foundSecKey := false
 			foundSizeKey := false
-			for i := int64(0); i <= diff; i++ {
-				sec := min + i
-				secKey := SecStatsKey(PopStatsKey(uqdq1.GetName()), sec)
-				secExists, _ := redis.Bool(conn.Do("EXISTS", secKey))
-				if secExists {
-					foundSecKey = true
 
-					// we sleep after popping so we can
-					// be sure both pop were flushed
-					popStats, _ := redis.Int64(conn.Do("GET", secKey))
-					So(popStats, ShouldEqual, 2)
-				}
+			secKey := SecStatsKey(PopStatsKey(uqdq1.GetName()), now)
+			secExists, _ := redis.Bool(conn.Do("EXISTS", secKey))
+			if secExists {
+				foundSecKey = true
 
-				// there should be queue size logged for uqdq1
-				sizeKey := SecQueueSizeKey(uqdq1.GetName(), sec)
-				sizeExists, _ := redis.Bool(conn.Do("EXISTS", sizeKey))
-				if sizeExists {
-					foundSizeKey = true
+				// we sleep after popping so we can
+				// be sure both pop were flushed
+				popStats, _ := redis.Int64(conn.Do("GET", secKey))
+				So(popStats, ShouldEqual, 2)
+			}
 
-					// we pushed trice and popped twice so
-					// uqdq1 should have one size
-					sizeStats, _ := redis.Int64(conn.Do("GET", sizeKey))
-					So(sizeStats, ShouldEqual, 1)
-				}
+			// there should be queue size logged for uqdq1
+			sizeKey := SecQueueSizeKey(uqdq1.GetName(), now)
+			sizeExists, _ := redis.Bool(conn.Do("EXISTS", sizeKey))
+			if sizeExists {
+				foundSizeKey = true
+
+				// we pushed twice and popped twice so
+				// uqdq1 should have zero size
+				sizeStats, _ := redis.Int64(conn.Do("GET", sizeKey))
+				So(sizeStats, ShouldEqual, 0)
 			}
 
 			So(foundSecKey, ShouldEqual, true)
